390ad062dd32d903db939db074813bb2aeade10d
unknown <rouchashoori@gmail.com>
2025-06-25 23:15:56 +0330

    add card app to installed apps

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'card.apps.CardConfig',
 ]
 
 MIDDLEWARE = [
